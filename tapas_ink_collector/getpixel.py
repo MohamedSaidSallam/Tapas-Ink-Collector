2c59099b2ff3d1713b55078d0bd6c26010c87ef6
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-09-03 10:10:04 +0200

    feat: stop saving img to file

--- tapas_ink_collector/getpixel.py
+++ tapas_ink_collector/getpixel.py
@@ -1,21 +1,13 @@
-import os
+import io
 
 import numpy
 from PIL import Image
-
-from tapas_ink_collector.config_loader import config
-
-os.makedirs(config["outputFolder"], exist_ok=True)
-TEMP_IMG_PATH = f'{config["outputFolder"]}/screen.png'
 
 
 def getPixel(device, x, y):
     image = device.screencap()
 
-    with open(TEMP_IMG_PATH, 'wb') as f:
-        f.write(image)
-
-    image = Image.open(TEMP_IMG_PATH)
+    image = Image.open(io.BytesIO(image))
     image = numpy.array(image, dtype=numpy.uint8)
 
     return image[y][x]
